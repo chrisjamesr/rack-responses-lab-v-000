51a1729f4738b534e66c0d4cb7f92b47a90aca40
Chris Rudolph <chrisjames.r@gmail.com>
2017-10-18 01:14:16 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+class Application
+
+  def greeting
+    resp = Rack::Response.new
+    
+    if Time.now.hour > 12
+      resp.write "Good Morning"
+    else
+      resp.write "Good Afternoon"
+    end
+    resp.finish
+  end
+
+end  #  End of Class    
